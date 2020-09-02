45902e4f4838b74746d9d440ec90637b6dfc47a7
anthonyophernandez <anthonyophernandez@gmail.com>
2020-09-02 10:25:11 +0200

    Filter notes - arePinned & areNotPinned

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -227,12 +227,29 @@
     </aside>
     <main class="fixed inset-0 px-2 mt-16 overflow-y-auto" :class="(isMenuDisplayed) ? 'ml-20 sm:ml-64 sm:z-40' : 'ml-20'">
       <TakeNote class="mx-auto mt-2 mb-8" :tags="tags"/>
-      <div class="grid gap-5 mt-2 mb-20" :class="(isDisplayedGrid) ? 'xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  md:mx-8 sm:mx-0' : 'grid-cols-1'">
+      <div v-show="arePinned.length > 0" class="w-full text-center text-xs font-semibold text-gray-500">PINNED</div>
+      <div class="grid gap-5 mt-4 mb-4" :class="(isDisplayedGrid) ? 'xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  md:mx-8 sm:mx-0' : 'grid-cols-1'">
         <NoteCard
           :ref="'note-'+ index"
           class="w-auto h-auto mx-auto"
           :class="openedNoteClass(index)"
-          v-for="(note, index) in notes" :key="index"
+          v-for="(note, index) in arePinned" :key="index"
+          :index="index"
+          :note="note"
+          @open="openNote(index)"
+          @select="addNoteToSelected"
+          @unselect="removeNoteFromSelected"
+          @close="closeNote"
+          :tags="tags"
+        />
+      </div>
+      <div v-show="arePinned.length > 0" class="w-full text-center text-xs font-semibold text-gray-500">OTHERS</div>
+      <div class="grid gap-5 mt-4 mb-20" :class="(isDisplayedGrid) ? 'xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  md:mx-8 sm:mx-0' : 'grid-cols-1'">
+        <NoteCard
+          :ref="'note-'+ index"
+          class="w-auto h-auto mx-auto"
+          :class="openedNoteClass(index)"
+          v-for="(note, index) in areNotPinned" :key="index"
           :index="index"
           :note="note"
           @open="openNote(index)"
@@ -299,7 +316,13 @@ export default {
     ...mapState({
       notes: state => state.notes,
       tags: state => state.tags
-    })
+    }),
+    arePinned () {
+      return this.notes.filter(n => n.isPinned)
+    },
+    areNotPinned () {
+      return this.notes.filter(n => !n.isPinned)
+    }
   },
   methods: {
     selectedSectionClass (section) {
